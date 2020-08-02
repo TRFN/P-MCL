c1c23725b0784888f1a96b0839f4638c75225cd7
TRFN <tulio.nasc95@gmail.com>
2020-08-02 13:50:20 -0300

    Criado sistema de sessões. Atualizado sistema DB. Falta documentar o SYS

--- app/motor/classes/database.php
+++ app/motor/classes/database.php
@@ -56,7 +56,11 @@
         }
 
         public function escrever($chave, $valor){
-            $this->dados[$chave] = $valor;
+            if($chave==-1){
+                $this->dados[] = $valor;
+            } else {
+                $this->dados[$chave] = $valor;
+            }
         }
 
         public function ler($chave="*",$obj=false,$reload=false){
@@ -68,7 +72,6 @@
 
         public function gravar(){
             $dados = serialize($this->dados);
-            echo "$this->dir/{$this->database}.db";
             $saida = file_put_contents("$this->dir/{$this->database}.db",$this->senha==-1?$dados:$this->codificar($dados,$this->senha));
             $this->atualizar();
             return $saida;
