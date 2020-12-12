53958ecfe363ddcfa26bc91c748f7f0ab1d446b3
TRFN <tulio.nasc95@gmail.com>
2020-12-12 00:47:30 -0300

    Uploader

--- app/motor/classes/uploader.php
+++ app/motor/classes/uploader.php
@@ -1,101 +1,30 @@
 <?php
     class uploader {
-        private $db = null;
-        private $param = null;
-        private $exts = array();
-        private $id = null;
-        private $dir = "";
-        private $valido = false;
-        private $iext = "";
+        private $dir = null;
 
         function __construct($security=-1){
-            $this->db = new database("uploader-control", $security);
             $this->dir = dirname(dirname(__DIR__)) . "/uploads";
         }
 
-        public function ler($param){
-            $this->param = $param;
+        public function file($name){
+            return $this->dir . "/{$name}.b64";
         }
 
-        public function ext($exts){
-            if(is_string($exts)): $exts = array($exts); endif;
-            $this->exts = $exts;
+        public function read($name){
+            return $this->exist($name) ? file_get_contents($this->file($name)):"notfound";
         }
 
-        public function id($id){
-            $this->id = $id;
+        public function upload($name,$data){
+            return file_put_contents($this->file($name),$data);
         }
 
-        public function valido(){
-            $iext = strtolower(pathinfo(basename($_FILES[$this->param]["name"]),PATHINFO_EXTENSION));
-            $correct = false;
-
-            foreach($this->exts as $ext){
-                $correct = $correct || ((string)$iext === (string)$ext);
-            }
-
-            if ($_FILES[$this->param]["size"] > 100000000 || !$correct){
-                return false;
-            }
-
-            $id = explode("/", $this->id);
-            $myid = array_pop($id);
-            $dir = implode("/",$id);
-
-            mkdir("{$this->dir}/{$dir}");
-
-            $this->iext = $iext;
-            // die(print_r($_FILES,true));
-            // die(print_r($this,true));
-            return ($this->valido=move_uploaded_file($_FILES[$this->param]["tmp_name"], ($this->arquivosalvo="{$this->dir}/{$dir}/{$myid}.{$iext}")));
+        public function exist($name){
+            return file_exists($this->file($name));
         }
 
-        public function upload(){
-            if($this->valido){
-                $this->db->escrever($this->id, array(
-                    "mime" => $_FILES[$this->param]["type"],
-                    "ext" => $this->iext,
-                    "arquivo" => $this->arquivosalvo,
-                    "data" => array(date("d"),date("m"),date("Y"))
-                ));
-                $this->db->gravar();
-            } else {
-                // die(print_r($_FILES,true));
-            }
-
-            $this->valido = false;
-            $this->iext = "";
-            $this->arquivosalvo = "";
-            $this->param = null;
-            $this->exts = array();
-            $this->id = null;
-        }
-
-        public function existe($id){
-            return !($this->db->ler($id) === null);
-        }
-
-        public function dados($id,$obj = true){
-            return $this->db->ler($id, $obj);
-        }
-
-        public function apagar($id){
-            if($this->existe($id)){
-                unlink($this->dados($id)->arquivo);
-                $this->db->escrever($id, null);
-                $this->db->gravar();
-            }
-        }
-
-        public function display($id){
-            if($this->existe($id)){
-                $arq = $this->db->ler($id);
-                header("Content-Type: {$arq["tipo"]}");
-                readfile($arq["arquivo"]);
-                exit;
-            } else {
-                header("Content-Type: text/plain");
-                exit("Este arquivo não pôde ser encontrado!");
+        public function unlink($name){
+            if($this->exist($name)){
+                unlink($this->file($name));
             }
         }
     }
